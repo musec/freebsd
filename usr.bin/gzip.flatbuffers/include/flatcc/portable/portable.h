de31dedd59a624a6b3fbdc26ce9bb59d52268749
Brian Kidney <bkidney@briankidney.ca>
2016-10-24 19:56:29 -0230

    Adds dependencies required for flatbuffers.

---
+++ usr.bin/gzip.flatbuffers/include/flatcc/portable/portable.h
@@ -0,0 +1,23 @@
+#ifndef PORTABLE_H
+#define PORTABLE_H
+
+#include "pversion.h"
+#include "pwarnings.h"
+#include <assert.h>
+#if (!defined(__STDC_VERSION__) || __STDC_VERSION__ < 201112L) && !defined (PORTABLE_STANDALONE)
+#include "pinttypes.h"
+#include "pstdalign.h"
+#include "pinline.h"
+#include "pstatic_assert.h"
+#else
+#include <inttypes.h>
+#include <stdalign.h>
+#ifndef static_assert
+#define static_assert(pred, msg) _Static_assert(pred, msg)
+#endif
+#endif
+
+#include "pendian.h"
+#include "punaligned.h"
+
+#endif /* PORTABLE_H */
---
+++ usr.bin/gzip.flatbuffers/include/flatcc/portable/pparsefp.h
@@ -0,0 +1,65 @@
+#ifndef PPARSEFP_H
+#define PPARSEFP_H
+
+/*
+ * Parses a float or double number and returns the length parsed if
+ * successful. The length argument is of limited value due to dependency
+ * on `strtod` - buf[len] must be accessible and must not be part of
+ * a valid number, including hex float numbers..
+ *
+ * Unlike strtod, whitespace is not parsed.
+ *
+ * May return:
+ * - null on error,
+ * - buffer start if first character does not start a number,
+ * - or end of parse on success.
+ *
+ */
+#include <math.h> /* for HUGE_VAL */
+
+#define PDIAGNOSTIC_IGNORE_UNUSED_FUNCTION
+#include "pdiagnostic_push.h"
+
+#if defined(_MSC_VER) && !defined(isinf)
+#include <float.h>
+#define isnan _isnan
+#define isinf(x) (!_finite(x))
+#endif
+
+#ifndef PORTABLE_USE_GRISU3
+#define PORTABLE_USE_GRISU3 1
+#endif
+
+#if PORTABLE_USE_GRISU3
+#include "grisu3_parse.h"
+#endif
+
+#ifdef grisu3_parse_double_is_defined
+static inline const char *parse_double(const char *buf, int len, double *result)
+{
+    return grisu3_parse_double(buf, len, result);
+}
+#else
+#include <stdio.h>
+static inline const char *parse_double(const char *buf, int len, double *result)
+{
+    char *end;
+
+    (void)len;
+    *result = strtod(buf, &end);
+    return end;
+}
+#endif
+
+static inline const char *parse_float(const char *buf, int len, float *result)
+{
+    const char *end;
+    double v;
+
+    end = parse_double(buf, len, &v);
+    *result = (float)v;
+    return end;
+}
+
+#include "pdiagnostic_pop.h"
+#endif /* PPARSEFP_H */
